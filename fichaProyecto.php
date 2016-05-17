ca1f79a23df4af36928ff8f08e16de79948b1460
VGS <Vic@MacBook-Pro-de-VGS.local>
2016-05-17 16:57:56 +0200

    Etiquetas reales.

--- fichaProyecto.php
+++ fichaProyecto.php
@@ -42,11 +42,11 @@ echo $idProyecto;
             $(function () {
             $('#container').highcharts({
         title: {
-            text: 'Monthly Average Temperature',
+            text: 'Evaluaciones Totales',
             x: -20 //center
         },
         subtitle: {
-            text: 'Source: WorldClimate.com',
+            text: 'Tienda Online',
             x: -20
         },
         xAxis: {
@@ -55,7 +55,7 @@ echo $idProyecto;
         },
         yAxis: {
             title: {
-                text: 'Temperature (°C)'
+                text: 'Puntuaciones'
             },
             plotLines: [{
                 value: 0,
@@ -64,7 +64,7 @@ echo $idProyecto;
             }]
         },
         tooltip: {
-            valueSuffix: '°C'
+            valueSuffix: ' Puntos'
         },
         legend: {
             layout: 'vertical',
@@ -73,17 +73,23 @@ echo $idProyecto;
             borderWidth: 0
         },
         series: [{
-            name: 'Tokyo',
+            name: 'Responsabilidad',
             data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
         }, {
-            name: 'New York',
+            name: 'Estrategia',
             data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
         }, {
-            name: 'Berlin',
-            data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
+            name: 'Adquisición',
+            data: [-0.9, 0.6, 3.5, 5.4, 13.5, 5.0, 18.6, 17.9, 7.3, 9.0, 3.9, 1.0]
         }, {
-            name: 'London',
+            name: 'Rendimiento',
             data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+        }, {
+            name: 'Conformidad',
+            data: [-0.9, 0.6, 3.5, 8.4, 13.5, 15.0, 5.6, 17.9, 14.3, 9.0, 3.9, 1.0]
+        }, {
+            name: 'Conducta Humana',
+            data: [-0.9, 0.6, 3.5, 8.4, 1.5, 13.0, 18.6, 17.9, 14.3, 2.0, 3.9, 1.0]
         }]
     });
 });
