8ff0a5f645d222a700ada06e1c13d291a2736416
VGS <Vic@MacBook-Pro-de-VGS.local>
2016-05-20 20:20:21 +0200

    Añadidos archivos para hacer el borrado de los objetivos y principios. Falta terminar. Añadidos archivos para insertar principios y objetivos.

---
+++ borrarobjetivo.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Registro eliminado.</title>
+<META name='robot' content='noindex, nofollow'>
+</head>
+
+<body>
+
+<?php
+
+// Actualizamos en funcion del id que recibimos
+
+		$id = $_POST['id'];
+
+ 		$mysqli = mysqli_connect("localhost","root","root", "test");
+		$query = "DELETE * FROM $objetivos where id = '$id'"; 
+		$result = mysql_query($query);   
+
+echo "
+<p>El objetivo ha sido eliminado con exito.</p>
+
+<p><a href='javascript:history.go(-1)'>VOLVER ATRÁS</a></p>
+";
+
+?>
+
+
+
+</body>
+
+</html> 
